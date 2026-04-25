2e59f4ab5b31e558cb8ab905f0fc3d88c01bc10a
salemjadroun <akrounanim@gmail.com>
2026-04-25 18:03:08 +0100

    EX2 : Write print_hex function and implement the second challenge

--- set_1/EX_2.c
+++ set_1/EX_2.c
@@ -9,3 +9,30 @@ uint8_t *xor_bytes(uint8_t *a, uint8_t *b, size_t len)
     return out;
 }
 
+void print_hex(uint8_t *bytes, size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+        printf("%02x", bytes[i]);
+    printf("\n");
+}
+
+
+int challenge_2(){
+  char *hexa = "1c0111001f010100061a024b53535009181c";
+  char *hexb = "686974207468652062756c6c277320657965";
+  
+  size_t len_a, len_b;
+  
+  uint8_t *a = hex_to_bytes(hexa, &len_a);
+  uint8_t *b = hex_to_bytes(hexb, &len_b);
+
+  uint8_t *xored = xor_bytes(a,b,len_a);
+  
+  print_hex(xored,len_a);
+  free(a);
+  free(b);
+  free(xored);
+
+  return 0;
+
+}
--- set_1/EX_2.h
+++ set_1/EX_2.h
@@ -4,6 +4,7 @@
 #include "EX_1.h"
 
 uint8_t *xor_bytes(uint8_t *a, uint8_t *b, size_t len);
-
+void print_hex();
+int challenge_2();
 
 #endif
