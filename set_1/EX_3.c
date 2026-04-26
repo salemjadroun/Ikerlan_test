38c951e3f48801db9b55614c76bfe8a227e6241e
salemjadroun <akrounanim@gmail.com>
2026-04-26 07:29:31 +0100

    EX4 : edit EX3 to be usable for our work on ex4

--- set_1/EX_3.c
+++ set_1/EX_3.c
@@ -20,27 +20,32 @@ float english_score(uint8_t *bytes, size_t len)
     return score / len;
 }
 
-int challenge_3()
+float best_single_xor(uint8_t *bytes, size_t len, uint8_t *best_key)
 {
-    
-    char *hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    size_t len;
-    uint8_t *bytes = hex_to_bytes(hex, &len);
-
     float best_score = 0.0;
-    uint8_t best_key = 0;
     for (uint8_t key = 0; key < 255; key++) {
         uint8_t *xored = single_byte_xor(bytes, len, key);
         float score = english_score(xored, len);
         if (score > best_score) {
             best_score = score;
-            best_key = key;
+            *best_key = key;
         }
         free(xored);
     }
+    return best_score;
+}
 
-    printf("Best key: %d\n", best_key);
+int challenge_3()
+{
+    char *hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    size_t len;
+    uint8_t *bytes = hex_to_bytes(hex, &len);
+    
+    uint8_t best_key;
+    best_single_xor(bytes, len, &best_key);
+    
     uint8_t *decrypted = single_byte_xor(bytes, len, best_key);
+    printf("Best key: %d\n", best_key);
     printf("Decrypted: %s\n", decrypted);
     free(decrypted);
     free(bytes);
--- set_1/EX_3.h
+++ set_1/EX_3.h
@@ -5,6 +5,7 @@
 
 uint8_t *single_byte_xor(uint8_t *bytes, size_t len, uint8_t key);
 float english_score(uint8_t *bytes, size_t len);
+float best_single_xor(uint8_t *bytes, size_t len, uint8_t *best_key);
 int challenge_3();
 
 
