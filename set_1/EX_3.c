1c67d23f32c0684354daf27f9503e6d774f35771
salemjadroun <akrounanim@gmail.com>
2026-04-26 06:53:55 +0100

    EX3 : implement the main function of challenge 3

--- set_1/EX_3.c
+++ set_1/EX_3.c
@@ -20,3 +20,28 @@ float english_score(uint8_t *bytes, size_t len)
     return score / len;
 }
 
+int challenge_3()
+{
+    
+    char *hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    size_t len;
+    uint8_t *bytes = hex_to_bytes(hex, &len);
+
+    float best_score = 0.0;
+    uint8_t best_key = 0;
+    for (uint8_t key = 0; key < 255; key++) {
+        uint8_t *xored = single_byte_xor(bytes, len, key);
+        float score = english_score(xored, len);
+        if (score > best_score) {
+            best_score = score;
+            best_key = key;
+        }
+        free(xored);
+    }
+
+    printf("Best key: %d\n", best_key);
+    uint8_t *decrypted = single_byte_xor(bytes, len, best_key);
+    printf("Decrypted: %s\n", decrypted);
+    free(decrypted);
+    free(bytes);
+}
--- set_1/main.c
+++ set_1/main.c
@@ -1,10 +1,12 @@
-#include "EX_2.h"
+#include "EX_3.h"
 
 int main(){
   printf("executing challenge 1...\n");
   challenge_1();
   printf("executing challenge 2...\n");
   challenge_2();
+  printf("executing challenge 3...\n");
+  challenge_3();
   return 0;
   
 }
